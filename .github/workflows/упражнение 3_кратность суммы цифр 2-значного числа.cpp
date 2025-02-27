341e29317462960f4712e218955d4c1f1aba8ca4
EmelyanovPavel <gdetoty@list.ru>
2025-02-27 11:33:34 +0300

    Create упражнение 3_кратность суммы цифр 2-значного числа.cpp

---
+++ .github/workflows/упражнение 3_кратность суммы цифр 2-значного числа.cpp
@@ -0,0 +1,40 @@
+//11) определить, кратна ли трем сумма цифр двухзначного числа
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+
+   int number1, number2;
+   cin >> number1 >> number2;
+
+	cout << "Created number: " << number1 << number2 << endl;
+
+	if ((number1 + number2) % 3 == 0) cout << "Yes";
+	else cout << "No";
+
+	return 0;
+}
+
+//12) определить, кратна ли числу А сумма цифр двухзначного числа
+ 
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+
+   int number1, number2, a;
+   cin >> number1 >> number2;
+	cin >> a;
+
+	cout << "Created number: " << number1 << number2 << endl;
+
+	if ((number1 + number2) % a == 0) cout << "Yes";
+	else cout << "No";
+
+	return 0;
+}
