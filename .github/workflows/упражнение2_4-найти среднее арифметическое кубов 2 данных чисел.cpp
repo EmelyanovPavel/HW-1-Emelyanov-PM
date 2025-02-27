c54c4a636be948cc114256381eac93b1a4a6037c
EmelyanovPavel <gdetoty@list.ru>
2025-02-27 10:24:02 +0300

    Create упражнение2_4-найти среднее арифметическое кубов 2 данных чисел.cpp

---
+++ .github/workflows/упражнение2_4-найти среднее арифметическое кубов 2 данных чисел.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() 
+ {
+
+    double a, b;
+    cin >> a >> b;
+
+    auto avg = (a * a * a + b * b * b) / 2.0;
+
+    cout << avg << '\n';
+
+    return 0;
+}
