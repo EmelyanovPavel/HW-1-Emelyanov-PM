38b2613aecae1df7f4ec34fa55d08697768f8d15
EmelyanovPavel <gdetoty@list.ru>
2025-02-27 11:21:13 +0300

    Create упражнение 3_8.cpp

---
+++ .github/workflows/упражнение 3_8.cpp
@@ -0,0 +1,19 @@
+//8) определить, одинаковы ли числа данного двухзначного числа.
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+
+   int number1, number2;
+   cin >> number1 >> number2;
+
+   cout << "Created number: " << number1 << number2 << endl;
+
+   if (number1 == number2) cout << "Yes" << endl;
+   else cout << "No" << endl;
+
+   return 0;
+}
