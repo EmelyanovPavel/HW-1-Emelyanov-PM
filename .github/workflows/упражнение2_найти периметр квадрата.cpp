8edb2fd25808fd78318f021d035c61942fba5bf6
EmelyanovPavel <gdetoty@list.ru>
2025-02-27 09:57:38 +0300

    упражнение2_1-найти периметр квадрата.cpp

---
+++ .github/workflows/упражнение2_найти периметр квадрата.cpp
@@ -0,0 +1,20 @@
+// найти периметр квадрата, площадь которого равна а.
+#include <cstdio>
+#include <iostream> 
+#include <cmath>
+
+using namespace std;
+
+int main() 
+{
+ 
+	int a;
+	cout << "Enter a square value: " << endl;
+	cin >> a;
+
+	int perimeter = sqrt(a) * 4;
+
+	cout << "Perimeter = " << perimeter << endl;
+
+	return 0;
+}
