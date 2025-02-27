563ebf4d1d1d5cc0d4ea830b51bc1f237f1f9a99
EmelyanovPavel <gdetoty@list.ru>
2025-02-27 11:28:20 +0300

    Create упражнение 3_сумма чисел 2-значного числа.cpp

---
+++ .github/workflows/упражнение 3_сумма чисел 2значного числа.cpp
@@ -0,0 +1,39 @@
+//9) определить, является ли сумма двузначного числа четной
+ 
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+
+   int number1, number2;
+   cin >> number1 >> number2;
+
+	cout << "Created number: " << number1 << number2 << endl;
+
+	if ((number1 + number2) % 2 == 0) cout << "Yes";
+	else cout << "No";
+
+	return 0;
+}
+
+//10) определить, является ли сумма двузначного числа нечетной
+ 
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+
+   int number1, number2;
+   cin >> number1 >> number2;
+
+	cout << "Created number: " << number1 << number2 << endl;
+
+	if ((number1 + number2) % 2 != 0) cout << "Yes";
+	else cout << "No";
+
+	return 0;
+}
